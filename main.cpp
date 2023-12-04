1cb9186e6aa01889d8f2e06c0b6d2f2ba072d8ed
HeidiG12 <hguntert@ufl.edu>
2023-12-04 15:48:35 -0500

    working on menu

--- main.cpp
+++ main.cpp
@@ -2,58 +2,73 @@
 using namespace std;
 #include "QuickSort.h"
 
-int main() {
+int main()
+{
 
-    //Start menu
+    // Start menu
     cout << "Hello and welcome to Smooth Skies, an interactive program that aims to inform "
-            "users on the airports to avoid!" << endl;
+            "users on the airports to avoid!"
+         << endl;
 
-    //boolean for program exit
+    // boolean for program exit
     bool exitProgram = false;
 
+    while (!exitProgram)
+    {
 
-    while(!exitProgram){
-
-        //allows user to choose different sorting methods
+        // allows user to choose different sorting methods
         int sortOption = 0;
         bool quickSort = false;
         bool mergeSort = false;
 
-        //menu
+        // menu
         cout << "Please Select an Option:\n"
                 "1. Quick Sort\n"
                 "2. Merge Sort\n"
-                "3. Exit" << endl;
+                "3. Exit"
+             << endl;
 
-        //Second Menu Depending on user's first choice
+        // Second Menu Depending on user's first choice
         cin >> sortOption;
-        switch (sortOption){
-            case 1:
-                quickSort = true;
-                break;
-            case 2:
-                mergeSort = true;
-                break;
-            case 3:
-                // Reverse order logic
-                break;
-            case 4:
-                exitProgram = true;
-                break;
-            default:
-                cout << "Invalid option. Please try again." << endl;
+        switch (sortOption)
+        {
+        case 1:
+            quickSort = true;
+            break;
+        case 2:
+            mergeSort = true;
+            break;
+        case 3:
+            // Reverse order logic
+            break;
+        case 4:
+            exitProgram = true;
+            break;
+        default:
+            cout << "Invalid option. Please try again." << endl;
         }
 
-        //reads in data for appropriate method
-        if(quickSort){
+        // reads in data for appropriate method
+        if (quickSort)
+        {
             QuickSort quick;
             quick.readInData();
         }
-        else if (mergeSort){
-
+        else if (mergeSort)
+        {
+            // MergeSort merge;
+            // merge.readInData();
         }
 
-        //Allows user to sort airports depending on:
+        int attributeOption = 0;
+        bool numLateFlights = false;
+        bool numSecDelays = false;
+        bool numWeatherDelays = false;
+        bool numFlightsCancelled = false;   
+        bool numDelayedFlights = false;
+        bool numFlightsTotaled = false;
+        bool numMinutesDelayed = false;
+        // Allows user to sort airports depending on:
         cout << "Which attributes do you want to sort?\n"
                 "1. Number of Late Flights\n"
                 "2. Number of Security Delays\n"
@@ -63,6 +78,34 @@ int main() {
                 "6. Number of Flights Totaled\n"
                 "7. Number of Minutes Delayed\n";
 
+        // Menu Depending on user's choice
+        cin >> attributeOption;
+        switch (attributeOption)
+        {
+        case 1:
+            numLateFlights = true;
+            break;
+        case 2:
+            numSecDelays = true;
+            break;
+        case 3:
+            numWeatherDelays = true;
+            break;
+        case 4:
+            numFlightsCancelled = true;
+            break;
+        case 5:
+            numDelayedFlights = true;
+            break;
+        case 6:
+            numFlightsTotaled = true;
+            break;
+        case 7:
+            numMinutesDelayed = true;
+            break;
+        default:
+            cout << "Invalid option. Please try again." << endl;
+        }
     }
     return 0;
 }
